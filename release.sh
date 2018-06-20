7b9fc8b9f1872989fd3f710d2803c51452b1e48f
s.bershadsky <nikitinsm@gmail.com>
2018-06-20 17:04:16 +0300

    FIX: release.sh

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 DIR="$(pwd)"
-VERSION="$(docker run -it -v $DIR:/repository bershadsky/gitversion /showvariable SemVer | sed 's/[^-+_.a-zA-Z0-9]//g')"
+VERSION="$(docker run --rm -v $DIR:/repository bershadsky/gitversion /showvariable SemVer | sed 's/[^-+_.a-zA-Z0-9]//g')"
 
 git tag ${VERSION}
 git push origin ${VERSION}
